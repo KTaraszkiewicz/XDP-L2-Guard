982be153d700ac94e2b5f46c3a85b19f98ccd2ba
Jozef Sztabinski <jozefsztabinski@gmail.com>
2026-06-02 22:11:16 +0200

    Use guard.sh in simple_ping test

--- scripts/simple_ping/block.sh
+++ scripts/simple_ping/block.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Terminal 3: Block 10.0.0.2
+# Terminal 3: Block 10.0.0.2 using guard.sh
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-echo -e "${RED}Blocking 10.0.0.2 in XDP map...${NC}"
-ip netns exec ns1 bpftool map update name blacklist_ips key hex 0a 00 00 02 value hex 00 00 00 00 00 00 00 00
-echo -e "${GREEN}Blocked.${NC}"
+echo -e "${RED}Blocking 10.0.0.2...${NC}"
+sudo ../../scripts/guard.sh -A -s 10.0.0.2 -j DROP
+echo -e "${GREEN}Done.${NC}"
